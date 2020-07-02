c2f0b1c9c2fba221f8d1868ef5fb16501fcb022f
reddyrajashekar <arreddy83@gmail.com>
2020-07-02 20:00:07 +0530

    code changes

--- src/test/java/stepDefinationFiles/SearchFlight.java
+++ src/test/java/stepDefinationFiles/SearchFlight.java
@@ -20,7 +20,6 @@ import java.util.concurrent.TimeUnit;
 public class SearchFlight {
     static WebDriver driver;
     public ReadProperties prop;
-    //Flight_Pages flight_pages=PageFactory.initElements(driver,Flight_Pages.class);
 
     @Given("Launch Browser")
     public void launch_browser() throws IOException {
@@ -34,7 +33,7 @@ public class SearchFlight {
     @When("Launch flights homepage")
     public void launch_flights_homepage() {
 try {
-    driver.get("https://in.via.com/");
+    driver.get("https://www.cleartrip.com/");
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
 }catch (WebDriverException e){
@@ -45,19 +44,31 @@ try {
     @When("I input source flight and destination flight")
     public void i_input_source_flight_and_destination_flight() {
         try {
-            Flight_Pages.From.sendKeys("Hyderabad");
-            Flight_Pages.To.sendKeys("Bangalore");
+            Flight_Pages flight_pages=PageFactory.initElements(driver,Flight_Pages.class);
 
+            flight_pages.Source.sendKeys("Hyderabad");
+           // flight_pages.From.click();
+            flight_pages.destination.sendKeys("Bangalore");
+          //  flight_pages.To.click();
         } catch (NoSuchElementException e) {
             e.printStackTrace();
         }
     }
 
-    @When("I Click on Seach button")
-    public void i_click_on_seach_button() {
-        Flight_Pages.DepartureDate.sendKeys("Wed, Jul 01 2020");
-        Flight_Pages.SearchFlightBtn.click();
-      }
+    @When("I Click on Search button")
+    public void i_click_on_search_button()throws Exception {
+        Flight_Pages flight_pages = PageFactory.initElements(driver, Flight_Pages.class);
+        try {
+            Thread.sleep(3000);
+            flight_pages.departure.sendKeys("Thu, 30 Jul, 2020");
+          //  flight_pages.CalenderIcon.click();
+            flight_pages.SelectDate.click();
+            Thread.sleep(3000);
+            flight_pages.SearchFlightBtn.click();
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Then("Search Results should displayed")
     public void search_results_should_displayed() {
