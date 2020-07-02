ebb9bfc8f1c5f7cdc2b85a15ee67bf781dd9e90f
reddyrajashekar <arreddy83@gmail.com>
2020-07-02 20:01:28 +0530

    code changes

--- src/main/java/Pages/Flight_Pages.java
+++ src/main/java/Pages/Flight_Pages.java
@@ -3,25 +3,65 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class Flight_Pages {
-   static WebDriver driver;
+   public WebDriver driver;
+/*
 
     @FindBy(xpath = "//input[@id=\'source\']")
+    public static WebElement Source;
+
+    @FindBy(xpath = "//span[contains(text(),'Hyderabad,Hyderabad')]")
     public static WebElement From;
 
-    @FindBy(xpath = "//input[@id='destination']")
-   public static WebElement To;
+    @FindBy(xpath = "//span[contains(text(),'Bangalore')]")
+    public static WebElement To;
+
+    @FindBy(xpath = "input[@id='destination']")
+    public static WebElement destination;
 
     @FindBy(xpath = "//div[@id='search-flight-btn']")
     public static WebElement SearchFlightBtn;
 
-    @FindBy(xpath = "//input[@id='departure']")
-   public static WebElement DepartureDate;
+    @FindBy(className = "calendar-icon")
+   public static WebElement CalenderIcon;
+
+    @FindBy(id = "departure")
+    public static WebElement departure;
+
+    @FindBy(xpath = "//div[@class='container']//div[3]//div[2]//div[3]//div[7]")
+    public static WebElement SelectDate;
+
+*/
+
+    @FindBy(xpath = "//input[@id='FromTag']")
+    public static WebElement Source;
+
+    @FindBy(xpath = "//span[contains(text(),'Hyderabad,Hyderabad')]")
+    public static WebElement From;
+
+    @FindBy(xpath = "//span[contains(text(),'Bangalore')]")
+    public static WebElement To;
+
+    @FindBy(xpath = "//input[@id='ToTag']")
+    public static WebElement destination;
+
+    @FindBy(xpath = "//input[@id='SearchBtn']")
+    public static WebElement SearchFlightBtn;
+
+    @FindBy(className = "calendar-icon")
+    public static WebElement CalenderIcon;
+
+    @FindBy(xpath = "//input[@id='DepartDate']")
+    public static WebElement departure;
+
+    @FindBy(xpath = "//a[contains(@class,'ui-state-default ui-state-active')]")
+    public static WebElement SelectDate;
+
+
 
     public Flight_Pages(WebDriver driver) {
        this.driver=driver;
-       PageFactory.initElements(driver,this);
+       //PageFactory.initElements(driver,this);
     }
 }
